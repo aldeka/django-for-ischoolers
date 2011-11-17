ab988b5fd289e17863dc77225ad58f8224060558
Karen Rustad <karen.rustad@gmail.com>
2011-11-17 02:16:50 -0800

    updated views.py

--- mysite/qandabear/views.py
+++ mysite/qandabear/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the questions index.")
+    
+def question(request, question_id):
+    return HttpResponse("You're looking at question %s." % (question_id,))
+
+def edit_answer(request, question_id, answer_id):
+    return HttpResponse("You're looking at the edit page for answer %s." % (answer_id,))
