94752a9190f83f23af6039f924dbf709d6de1a78
Karen Rustad <karen.rustad@gmail.com>
2011-11-17 16:56:20 -0800

    added our question and answer models

--- mysite/qandabear/models.py
+++ mysite/qandabear/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
